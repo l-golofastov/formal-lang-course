9fe79777f292b71e0ca23fedc98b7e0ea391bbd2
Lev-Golofastov <l.golofastov@mail.ru>
2023-11-19 16:47:49 +0300

    Fix bugs in bfs

--- project/regular_path_query.py
+++ project/regular_path_query.py
@@ -27,9 +27,26 @@ def rpq_tensor(regex, graph, start_nodes=None, final_nodes=None):
 
 
 def rpq_bfs(regex, graph, start_nodes=None, final_nodes=None, option=False):
-    result = {} if option else set()
+    # result = {} if option else set()
     dfa = create_dfa_by_regex(regex)
     nfa = create_nfa_by_graph(graph, start_nodes, final_nodes)
+    print(dfa.to_dict())
+
+    common_labels = set()
+    for _, label_dfa, _ in dfa:
+        for _, label_nfa, _ in nfa:
+            if label_dfa == label_nfa:
+                common_labels.add(label_dfa)
+
+    result = bfs_two_graph(dfa, nfa, common_labels, option)
+
+    return result
+
+
+def bfs_two_graph(dfa, nfa, common_labels, option=False):
+    result = {} if option else set()
+    if len(dfa.states) == 0 or len(nfa.states) == 0:
+        return result
 
     dfa_states_indexes = {state: index for index, state in enumerate(dfa.states)}
     dfa_indexes_states = {index: state for state, index in dfa_states_indexes.items()}
@@ -41,10 +58,6 @@ def rpq_bfs(regex, graph, start_nodes=None, final_nodes=None, option=False):
 
     direct_sum_decomposed = create_direct_sum_binary_matrices(dfa, nfa)
 
-    mask = scipy.sparse.dok_matrix((1, len(dfa.states) + len(nfa.states)), dtype=int)
-    for j in range(len(dfa.states) + len(nfa.states)):
-        mask[0, j] = 1
-
     for start_state_dfa in dfa.start_states:
         for start_state_nfa in nfa.start_states:
             if option:
@@ -52,38 +65,98 @@ def rpq_bfs(regex, graph, start_nodes=None, final_nodes=None, option=False):
             start_state_dfa_num = dfa_states_indexes[start_state_dfa]
             start_state_nfa_num = nfa_states_indexes[start_state_nfa]
 
-            front_prev = scipy.sparse.dok_matrix(
+            mask = scipy.sparse.dok_matrix(
                 (1, len(dfa.states) + len(nfa.states)), dtype=int
             )
+            for j in range(len(dfa.states) + len(nfa.states)):
+                mask[0, j] = 1
+
             front = scipy.sparse.dok_matrix(
                 (1, len(dfa.states) + len(nfa.states)), dtype=int
             )
-            front[0, start_state_dfa_num] = 1
-            front[0, start_state_nfa_num] = 1
+            front[0, start_state_dfa_num], front[0, start_state_nfa_num] = 1, 1
 
-            while not (np.array_equal(front_prev.toarray(), front.toarray())):
+            prev = 0
+            curr = mask.count_nonzero()
+
+            while prev != curr:
+                print(front.toarray())
                 for j in range(len(dfa.states) + len(nfa.states)):
                     front_element_reversed = 1 if front[0, j] == 0 else 0
                     mask[0, j] = 1 if (mask[0, j] * front_element_reversed > 0) else 0
+
+                front_dfa_prev = [
+                    (1 if front[0, j] != 0 else 0) for j in range(len(dfa.states))
+                ]
+                transition_labels_dfa = set()
 
                 multiplication_result = {}
                 for label in direct_sum_decomposed:
                     multiplication_result[label] = front @ direct_sum_decomposed[label]
 
-                front_prev = front
-                front = sum(multiplication_result.values())
-                for j in range(len(dfa.states) + len(nfa.states)):
-                    front[0, j] = 1 if (front[0, j] * mask[0, j] > 0) else 0
+                multiplication_sum_premature = sum(multiplication_result.values())
+                front_empty = scipy.sparse.dok_matrix(
+                    (1, len(dfa.states) + len(nfa.states)), dtype=int
+                )
+                front_premature = (
+                    front_empty
+                    if type(multiplication_sum_premature) == int
+                    else multiplication_sum_premature
+                )
+                front_dfa_curr = [
+                    (1 if front_premature[0, j] != 0 else 0)
+                    for j in range(len(dfa.states))
+                ]
 
-                for j in range(len(dfa.states), front.shape[1]):
-                    if front[0, j] == 1:
-                        if nfa_indexes_states[j] in nfa.final_states:
-                            if option:
-                                reachable_states.add(j - len(dfa.states))
-                            else:
-                                result.add(j - len(dfa.states))
+                for prev_dfa_state in range(len(front_dfa_prev)):
+                    for curr_dfa_state in range(len(front_dfa_curr)):
+                        if (
+                            front_dfa_prev[prev_dfa_state] == 1
+                            and front_dfa_curr[curr_dfa_state] == 1
+                        ):
+                            for state1, label, state2 in dfa:
+                                if (
+                                    state1 == dfa_indexes_states[prev_dfa_state]
+                                    and state2 == dfa_indexes_states[curr_dfa_state]
+                                    and label in common_labels
+                                ):
+                                    transition_labels_dfa.add(label)
+
+                multiplication_sum = scipy.sparse.dok_matrix(
+                    (1, len(dfa.states) + len(nfa.states)), dtype=int
+                )
+                for label in transition_labels_dfa:
+                    print(label)
+                    multiplication_sum += multiplication_result[label]
+                front = (
+                    front_empty
+                    if type(multiplication_sum) == int
+                    else multiplication_sum
+                )
+
+                for dfa_state in range(len(dfa.states)):
+                    for nfa_state in range(
+                        len(dfa.states), len(dfa.states) + len(nfa.states)
+                    ):
+                        if front[0, dfa_state] != 0 and front[0, nfa_state] != 0:
+                            if (
+                                dfa_indexes_states[dfa_state] in dfa.final_states
+                                and nfa_indexes_states[nfa_state] in nfa.final_states
+                            ):
+                                if option:
+                                    reachable_states.add(nfa_state - len(dfa.states))
+                                    print(reachable_states)
+                                else:
+                                    result.add(nfa_state - len(dfa.states))
+
+                prev = curr
+                curr = mask.count_nonzero()
 
             if option:
-                result[start_state_nfa_num] = reachable_states
+                result[start_state_nfa_num - len(dfa.states)] = reachable_states
 
     return result
+
+
+def bfs_two_graph_step():
+    return
